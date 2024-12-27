b12924259471f4953bf3776844f7bf88c5aa7639
AK261102 <ak503005@gmail.com>
2024-12-28 01:30:13 +0530

    creating that basic structure

---
+++ client/Square/Square.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import "./Square.css";
+const Square = () => {
+  return (
+    <div>Square</div>
+  )
+}
+
+export default Square
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './App.css';
+import Square from '../Square/Square';
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="main-div">
+      <div>
+        <h1 className="game-heading water-background">Tic Tac Toe</h1>
+        <div className="square-wrapper">
+          {renderFrom.map((arr, rowIndex) =>
+            arr.map((e, colIndex) => (
+              <Square key={`${rowIndex}-${colIndex}`} />
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
